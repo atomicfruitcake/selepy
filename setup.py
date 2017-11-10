3785e5768505f4cedf5c99a0cf29ab893f046fb8
atomicfruitcake <sam@wirewax.com>
2017-11-10 19:02:39 +0000

    Added license and pypi setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+'''!
+@author atomicfruitcake
+
+'''
+
+from distutils.core import setup
+setup(
+  name = 'selepy',
+  packages = ['selepy'],
+  version = '0.1',
+  description = 'Browser automation framework',
+  author = 'Sam Bass',
+  author_email = 'samjbass@hotmail.co.uk',
+  url = 'https://github.com/atomicfruitcake/selepy',
+  download_url = 'https://github.com/atomicfruitcake/selepy/archive/0.1.tar.gz',
+  keywords = ['selenium', 'logging', 'example'],
+  classifiers = [],
+)
