a01f418c36cb566495c5c219a2284dec08cb7537
atomicfruitcake <sam@wirewax.com>
2017-11-11 12:25:35 +0000

    Simplified structure

--- setup.py
+++ setup.py
@@ -6,14 +6,24 @@ Setup file for pypi
 
 from setuptools import setup
 setup(
-  name = 'selepy',
-  packages = ['selepy'],
-  version = '0.1.1',
-  description = 'Browser automation framework',
-  author = 'Sam Bass',
-  author_email = 'sam@wirewax.com',
-  url = 'https://github.com/atomicfruitcake/selepy',
-  download_url = 'https://github.com/atomicfruitcake/selepy/archive/0.1.1.tar.gz',
-  keywords = ['selenium', 'testing', 'automation', 'docker'],
-  classifiers = [],
+    name = 'selepy',
+    packages = ['selepy'],
+    version = '0.1.1',
+    description = 'Browser automation framework',
+    long_description='Browser automation framework designed to allow automation scripts '
+                     'to work both locally and inside docker containers',
+    license='MIT',
+    author = 'Sam Bass',
+    author_email = 'sam@wirewax.com',
+    url = 'https://github.com/atomicfruitcake/selepy',
+    download_url = 'https://github.com/atomicfruitcake/selepy/archive/0.1.1.tar.gz',
+    keywords = ['selenium', 'testing', 'automation', 'docker'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=['selenium'],
 )
